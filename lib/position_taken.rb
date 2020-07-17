614b68a89c8548f1fb19992f177e18f12ab0acb9
McKayla Lankau <lankaukk@yahoo.com>
2020-07-17 04:20:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  if board[index] == " " || "" || nil
+    return FALSE
+  elsif board[index] == "X" || "O"
+    return TRUE
+  end
+end
+
